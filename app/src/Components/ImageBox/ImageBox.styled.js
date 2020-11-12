cb50383e1637a6627f91fe43867227c973b4471f
codegingerine <urskosik@gmail.com>
2020-11-12 17:36:15 +0100

    chore: add backgroud imgage to ImageBox

--- app/src/Components/ImageBox/ImageBox.styled.js
+++ app/src/Components/ImageBox/ImageBox.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import logo from "Assets/png/bg.png";
 
 export const ImageBoxStyled = styled.a`
   display: flex;
@@ -8,6 +9,8 @@ export const ImageBoxStyled = styled.a`
   height: 190px;
   margin-bottom: 10px;
   overflow: hidden;
+  background: url(${logo}) no-repeat center;
+  background-size: 100px auto;
 `;
 
 export const ImageStyled = styled.img`
